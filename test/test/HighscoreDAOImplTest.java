b3bca507d9d7666512e90e142777c5281cab3592
dukocuk <duran.kse@gmail.com>
2018-05-02 20:15:52 +0200

    maked DAOException and tested further.

--- test/test/HighscoreDAOImplTest.java
+++ test/test/HighscoreDAOImplTest.java
@@ -1,10 +1,12 @@
 package test;
 
-
+import dao.DAOException;
 import dao.HighscoreDAO;
-import dao.entity.Highscore;
-import dao.impl.HighscoreDAOImpl;
+import dao.Highscore;
+import dao.HighscoreDAOImpl;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,23 +16,39 @@ import org.junit.Test;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author durankose
  */
 public class HighscoreDAOImplTest {
-    
+
     @Test
-    public void testGetAllScores() throws Exception{
-         HighscoreDAO dao = new HighscoreDAOImpl("jdbc:mysql://duko.mynetgear.com:3306/highscore","hemmeligbruger","hemmeligkode");
-         List<Highscore> scores = dao.getAllScores();
-         
-         Assert.assertNotNull(scores);
-         
-         for (Highscore score: scores){
-             System.out.println("Studienummer: " + score.getSnumber()+" Score: "+score.getScore());
-         }
+    public void testGetAllScores() throws Exception {
+        HighscoreDAO dao = new HighscoreDAOImpl("jdbc:mysql://duko.mynetgear.com:3306/highscore", "hemmeligbruger", "hemmeligkode");
+        List<Highscore> scores = dao.getAllScores();
+
+        Assert.assertNotNull(scores);
+        Assert.assertFalse(scores.isEmpty());
+
+        for (Highscore score : scores) {
+            System.out.println("Studienummer: " + score.getSnumber() + " Score: " + score.getScore());
+        }
     }
     
+    @Test
+    public void testAddScore()   {
+        try {
+            HighscoreDAO dao = new HighscoreDAOImpl("jdbc:mysql://duko.mynetgear.com:3306/highscore", "hemmeligbruger", "hemmeligkode");
+            Highscore highscore = new Highscore("s147153", 40);
+            dao.addScore(highscore);
+            
+            List<Highscore> scores = dao.getAllScores();
+            
+            Assert.assertNotNull(scores);
+        } catch (DAOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+
 }
