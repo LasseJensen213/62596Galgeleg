60641bb277afd4f827a31f00ea91239979f7ab45
lasse <lassejensen213@gmail.com>
2018-05-08 21:47:33 +0200

    GameState Serializable

--- src/galgeleg_server/GameState.java
+++ src/galgeleg_server/GameState.java
@@ -5,14 +5,21 @@
  */
 package galgeleg_server;
 
+import java.rmi.Remote;
 import java.util.ArrayList;
 
 /**
  *
  * @author Jimmy
  */
-public class GameState {
-    public String ordet;
+public class GameState implements Remote, java.io.Serializable{
+	
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 2L;
+	public String ordet;
     public String synligtOrd;
     public ArrayList<String> brugteBogstaver;
     public int antalForkerteBogstaver;
